aa9ff23e6c2f607039340beb593e02d9736c21cb
Rafael Borges <rafaelborges.av@gmail.com>
2022-08-17 19:48:45 -0300

    commit inicial

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Site em Construção</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+}
+</style>
